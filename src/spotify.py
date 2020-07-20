4b63cdd62e4ba81fd1f7ea6eb2d5495940ae0bbe
changboon <boon.heng@gmail.com>
2020-07-20 08:26:29 +0800

    added random sampling methods

--- src/spotify.py
+++ src/spotify.py
@@ -3,6 +3,10 @@ import spotipy
 import credentials
 from spotipy.oauth2 import SpotifyClientCredentials
 import urllib
+import json 
+import pandas as pd
+import time
+
 
 def load_and_auth_spotify(filepath):
 	creds = credentials.load_spotify_creds(filepath)
@@ -23,20 +27,28 @@ def spotify_search(sp, query):
 			"title": track['name'], 
 			"artist": track['artists'][0]['name'],
 			"album": track['album']['name'],
+			'release_date': track['album']['release_date'],
 			"uri": track['uri']
 		}
 	return False
 
 
-def get_spotify_uri(sp, row):
+def get_spotify_uri(sp, index, row):
 	query = row.title + " " + row.artist_name
 	record = spotify_search(sp, query)
 	if record == False:
 		query = row.title + " " + row.release
 		record = spotify_search(sp, query)
 		if record == False:
-			record = {"query": query, "title": "", "artist": "", "album": "", "uri": ""}
+			record = {"query": query, "title": "", "artist": "", "album": "", "release_date": "", "uri": ""}
 	record['track_id'] = row.track_id
+	if index%10000==0:
+		print(index)
+
+	with open('spotify_ids.csv', 'a', encoding="utf-8") as textfile:
+		record_feature = ["query", "title", "artist", "album", "release_date", "uri", "track_id"]
+		write_string = "|".join([record.get(feature) for feature in record_feature]) + "\n"
+		textfile.write(write_string)
 	return record
 		
 
@@ -52,12 +64,32 @@ def get_audio_analysis(sp, uri):
 def get_audio_features(sp, uris): 
 	if len(uris) > 100:
 		raise Exception("list's max size is 100")
-    audio_features = sp.audio_features(uris) 
-    audio_features_features = ['uri', 'danceability', 'energy', 'key', 'loudness', 'mode', 
-                               'speechiness', 'acousticness', 'instrumentalness', 
-                               'liveness', 'valence', 'tempo'
-                              ]
-    records = []
-    for rec in audio_features:
-        records.append({feature: rec.get(feature) for feature in audio_features_features})
-    return records
+	audio_features = sp.audio_features(uris)
+	audio_features_features = ['uri', 'danceability', 'energy', 'key', 'loudness', 'mode', 
+							'speechiness', 'acousticness', 'instrumentalness', 
+							'liveness', 'valence', 'tempo', 'time_signature'
+	]
+	records = []
+	bad_records = 0
+	for idx, rec in enumerate(audio_features):
+		try:
+			records.append({feature: rec.get(feature) for feature in audio_features_features})
+		except AttributeError:
+			record = {
+				'uri':uris[idx], 'danceability':-1, 'energy':-1, 'key':-1, 'loudness':-1, 'mode':-1, 
+							'speechiness':-1, 'acousticness':-1, 'instrumentalness':-1, 
+							'liveness':-1, 'valence':-1, 'tempo':-1, 'time_signature':-1
+			}
+			records.append(record)
+			bad_records += 1
+	if bad_records>0:
+		print("num bads:", bad_records)
+
+	df = pd.DataFrame(records)
+	filename = "./spotify_audio_features.csv"
+	if os.path.exists(filename):
+		df.to_csv(filename, mode='a', header=False, index=False)
+	else:
+		df.to_csv(filename, mode='a', header=True, index=False)
+	time.sleep(0.2)
+	return records
